efb189b83a7f1bf35f3635423890942c5d8d053b
SERVIDORES\matheusoliveira <matheusoliveira@inatel.br>
2017-05-15 16:35:30 -0300

    cria padrão de json simples

---
+++ runner.py
@@ -0,0 +1,23 @@
+import json
+
+if __name__ == '__main__':
+	file = open('patterns/simple-message.json', 'r+')
+
+	file_json =  json.load(file)
+
+	file.close()
+
+	print json.dumps(file_json, sort_keys=True)
+
+	name = raw_input('File name: ')
+	text = raw_input('Data to send: ')
+
+	new_json_file = open('contents/'+name, 'w+')
+
+	file_json['message']['text'] = text
+
+	new_json_file.write(json.dumps(file_json, indent=4))
+
+	new_json_file.close()
+
+	print json.dumps(file_json)
