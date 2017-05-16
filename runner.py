8b62a80221085bef8b4cb079b7ca6160e3c875cf
mugbug <zaronipedro@outlook.com>
2017-05-16 20:19:09 -0300

    corrigido erro ascii

--- runner.py
+++ runner.py
@@ -1,4 +1,7 @@
 import json
+import sys
+reload(sys)
+sys.setdefaultencoding('latin-1')
 
 if __name__ == '__main__':
 	file = open('patterns/simple-message.json', 'r+')
@@ -14,10 +17,12 @@ if __name__ == '__main__':
 
 	new_json_file = open('contents/'+name, 'w+')
 
-	file_json['message']['text'] = text
+	file_json['message']['text'] = str(text)
 
 	new_json_file.write(json.dumps(file_json, indent=4))
 
 	new_json_file.close()
 
-	print json.dumps(file_json)
+	data = json.dumps(file_json, ensure_ascii=False, encoding='utf-8')
+
+	print data.encode('latin-1').decode('utf-8')
